37663d6c5414f61caf1c7ca305dc791a32d06158
mike_ <mikezhifailiangzhihui@my.jcu.edu.au>
2023-03-25 11:31:40 +0800

    Initial commitP3

--- prac02/score_menu.py
+++ prac02/score_menu.py
@@ -1,4 +1,6 @@
 MENU="""(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"""
+HIGHEST_SCORE = 100
+LOWEST_SCORE = 0
 
 def main():
     print(MENU)
@@ -7,19 +9,29 @@ def main():
     while choice != "Q":
         choice = input("Choose one of the following option ").upper()
         if choice == "G":
-            score = int(input("Enter score: "))
+            score = get_valid_input("Enter score:", HIGHEST_SCORE, LOWEST_SCORE)
 
         elif choice == "P":
-            print(determine_status(score))
+            print(determine_grade(score))
 
-        elif choice=="S":
+        elif choice == "S":
             print(show_stars(score))
 
         else:
-            print("Farewell")
+            print("Invalid choice")
         print(MENU)
+    print("Farewell")
 
-def determine_status(score):
+
+def get_valid_input(prompt, high, low):
+    number = int(input(prompt))
+    while number < low or number > high:
+        print("Invalid score")
+        number = int(input(prompt))
+    return number
+
+
+def determine_grade(score):
     """Determine the status of a given score."""
     if score < 0 or score > 100:
         return "Invalid score"
@@ -31,8 +43,8 @@ def determine_status(score):
         return "Bad"
 
 def show_stars(score):
-    """str(stars)=("*" * int(score)"""
     stars="*" * score
     return stars
+
 
 main()
