ed1b320468e463fc813a606f6d23dd0274a60cb5
mike_ <mikezhifailiangzhihui@my.jcu.edu.au>
2023-04-08 10:07:02 +0800

    Initial commitP3

---
+++ prac04/total_income.py
@@ -0,0 +1,19 @@
+def main():
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_record(incomes)
+
+
+def print_record(incomes):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes, 1):
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+main()
